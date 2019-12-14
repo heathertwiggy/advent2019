194850c8bd372dce014320a18f004cd639d39ed7
axellj <axellj.dev@gmail.com>
2019-12-14 17:45:29 +0100

    DAY-1.2 solved!

--- src/days/Day1.java
+++ src/days/Day1.java
@@ -1,6 +1,7 @@
 package days;
 
 import component.FuelCounterUpper;
+import component.FuelCounterUpper2;
 import util.Input;
 
 public class Day1 implements Dayable {
@@ -13,6 +14,16 @@ public class Day1 implements Dayable {
     @Override
     public String computeOne() {
         return String.valueOf(new FuelCounterUpper(Input.list(1)).getFuelRequirement());
+    }
+
+    @Override
+    public String computeTwo() {
+        return String.valueOf(new FuelCounterUpper2(Input.list(1)).getFuelRequirement());
+    }
+
+    @Override
+    public String correctResultTwo() {
+        return "4973628";
     }
 
     @Override
